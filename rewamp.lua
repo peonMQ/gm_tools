a5ddc437bf2fcfb675cde3f456383ca730b90883
Aleksander Spro <abspro@gmail.com>
2023-09-06 15:56:34 +0200

    Add some gm fun scripts

---
+++ rewamp.lua
@@ -0,0 +1,36 @@
+local mq = require 'mq'
+local logger = require 'utils/logging'
+
+
+local function ensureTarget(targetId)
+  if not targetId then
+    logger.Debug("Invalid <targetId>")
+    return false
+  end
+
+  if mq.TLO.Target.ID() ~= targetId then
+    if mq.TLO.SpawnCount("id "..targetId)() > 0 then
+      mq.cmdf("/mqtarget id %s", targetId)
+      mq.delay("3s", function() return mq.TLO.Target.ID() == targetId end)
+    else
+      logger.Warn("EnsureTarget has no spawncount for target id <%d>", targetId)
+    end
+  end
+
+  return mq.TLO.Target.ID() == targetId
+end
+
+local npcs = mq.getFilteredSpawns(function(spawn) return spawn.Distance() < 1000
+                                                            and  spawn.Type() =="NPC"
+                                                            and spawn.Level() < 60
+                                                            and not string.find(spawn.Name(), "elven")
+                                                            and not string.find(spawn.Name(), "dwarven")
+                                                            and not string.find(spawn.Name(), "Kelynn")
+                                                          end)
+-- npcs = mq.getAllSpawns()
+for _, npc in ipairs(npcs) do
+  if ensureTarget(npc.ID()) then
+    mq.cmdf("/say #level 60")
+    mq.delay(1000, function() return mq.TLO.Target.Level() == 60 end)
+  end
+end
---
+++ zummon.lua
@@ -0,0 +1,37 @@
+local mq = require 'mq'
+local logger = require 'utils/logging'
+
+
+local function ensureTarget(targetId)
+  if not targetId then
+    logger.Debug("Invalid <targetId>")
+    return false
+  end
+
+  if mq.TLO.Target.ID() ~= targetId then
+    if mq.TLO.SpawnCount("id "..targetId)() > 0 then
+      mq.cmdf("/mqtarget id %s", targetId)
+      mq.delay("3s", function() return mq.TLO.Target.ID() == targetId end)
+    else
+      logger.Warn("EnsureTarget has no spawncount for target id <%d>", targetId)
+    end
+  end
+
+  return mq.TLO.Target.ID() == targetId
+end
+
+local npcs = mq.getFilteredSpawns(function(spawn) return spawn.Distance() < 3000
+                                                            and  spawn.Type() =="NPC"
+                                                            and spawn.Level() == 60
+                                                          end)
+-- npcs = mq.getAllSpawns()
+for i, npc in ipairs(npcs) do
+  if ensureTarget(npc.ID()) then
+    mq.cmdf("/say #summon")
+    mq.delay(1000, function() return mq.TLO.Target.Distance() < 100 end)
+  end
+
+  if i == 10 then
+    mq.exit()
+  end
+end
